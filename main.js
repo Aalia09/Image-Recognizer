7e612a18c760d3a7887e6f7ba7665c1fc6816fce
Aalia09 <76902204+Aalia09@users.noreply.github.com>
2021-06-15 15:36:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -21,3 +21,20 @@ function modelloaded()
 {
     console.log("Model Loaded");
 }
+
+function check()
+{
+    img = document.getElementById("captured_image");
+    classifier.classify(img , gotResult);
+}
+
+function gotResult(error , results)
+{
+    if(error) {
+        console.error(error);
+    }else{
+        console.log(results);
+        document.getElementById("name_of_object").innerHTML = results[0].label;
+        document.getElementById("accuracy_of_object").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
